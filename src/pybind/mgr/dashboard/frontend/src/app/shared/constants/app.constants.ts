707e81aac435dc864fd5d1aba597f5de6a045673
Pedro Gonzalez Gomez <pegonzal@redhat.com>
2024-03-01 13:43:12 +0100

    mgr/dashboard: ceph authenticate user from fs
    Resolves:rhbz#2271110
    
    Fixes: https://tracker.ceph.com/issues/64660
    Signed-off-by: Pedro Gonzalez Gomez <pegonzal@redhat.com>
    (cherry picked from commit 19a9f79b5f218d8cb757579bf8c0f5d610dde3a8)
    (cherry picked from commit dc6c64e76834337bcdf255c0eac9f457b95b1ff3)
    
     Conflicts:
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/cephfs/cephfs-tabs/cephfs-tabs.component.html
    	src/pybind/mgr/dashboard/frontend/src/app/shared/constants/app.constants.ts

--- src/pybind/mgr/dashboard/frontend/src/app/shared/constants/app.constants.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/constants/app.constants.ts
@@ -155,6 +155,7 @@ export class ActionLabelsI18n {
   CONNECT: string;
   DISCONNECT: string;
   RECONNECT: string;
+  AUTHORIZE: string;
 
   constructor() {
     /* Create a new item */
@@ -214,6 +215,7 @@ export class ActionLabelsI18n {
     this.FLAGS = $localize`Flags`;
     this.ENTER_MAINTENANCE = $localize`Enter Maintenance`;
     this.EXIT_MAINTENANCE = $localize`Exit Maintenance`;
+    this.AUTHORIZE = $localize`Authorize`;
 
     this.START_DRAIN = $localize`Start Drain`;
     this.STOP_DRAIN = $localize`Stop Drain`;
