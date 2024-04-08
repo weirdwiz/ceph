d9639b14fba7e3e7b6470f52eb41787b9cb074ff
Guillaume Abrioux <gabrioux@ibm.com>
2024-04-08 11:08:54 +0200

    ceph-volume: default type to 'bluestore' in 'raw list'
    This is a downstream patch only as it relates to "bluestore-rdr"
    changes which were introduced downstream only.
    
    Prior to 17.2.6-94, `ceph-bluestore-tool show-label` didn't return
    a key `type`.
    
    eg:
    
    ```
    {
        "/dev/vdj": {
                "osd_uuid": "1b817aa8-a266-4fd8-b056-2c2226bd916f",
                "size": 214748364800,
                "btime": "2024-04-08T08:44:33.542851+0000",
                "description": "main",
                "bfm_blocks": "52428800",
                "bfm_blocks_per_key": "128",
                "bfm_bytes_per_block": "4096",
                "bfm_size": "214748364800",
                "bluefs": "1",
                "ceph_fsid": "311a93fc-f476-11ee-a54a-525400479829",
                "ceph_version_when_created": "ceph version 17.2.6-100.el9cp (ea4e3ef8df2cf26540aae06479df031dcfc80343) quincy (stable)",
                "created_at": "2024-04-08T08:44:36.285703Z",
                "kv_backend": "rocksdb",
                "magic": "ceph osd volume v026",
                "mkfs_done": "yes",
                "osd_key": "AQBwrhNmM5V6NxAAUftSwl3v6ZGkWzkG8w2VjQ==",
                "ready": "ready",
                "type": "bluestore",   # <--- introduced after 17.2.6-94
                "whoami": "2"
            }
    }
    ```
    
    these labels are set by `ceph-osd` at the OSD creation time.
    It means any OSD created prior to 17.2.6-100 (the first downstream released version
    which ships the bluestore-rdr changes) doesn't have this information set.
    ceph-volume needs this key from 17.2.6-94 onwards in order to activate OSDs.
    
    The idea is to make it default to 'bluestore' when this key isn't found.
    
    Resolves: rhbz#2273936
    
    Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>

--- src/ceph-volume/ceph_volume/devices/raw/list.py
+++ src/ceph-volume/ceph_volume/devices/raw/list.py
@@ -42,7 +42,7 @@ def _get_bluestore_info(dev):
         if oj[dev]['description'] == 'main':
             whoami = oj[dev]['whoami']
             r.update({
-                'type': oj[dev]['type'],
+                'type': oj[dev].get('type', 'bluestore'),
                 'osd_id': int(whoami),
                 'ceph_fsid': oj[dev]['ceph_fsid'],
                 'device': dev,
