weirdwiz/ceph

commit fcfa6e797f021886971e3af97a5158797cd2d96f

cephadm: nvmeof backports
- commit 70c00e8ba787d9e9106934cfee0e0afa606ce326
  Author: Adam King <adking@redhat.com>
  Date:   Mon Jan 29 11:23:54 2024 -0500

    cephadm: fix get_version for nvmeof

    This needed to be using the container id it was
    passed, instead of ctx.image which is likely to
    be `None` when this is run.

    Fixes: https://tracker.ceph.com/issues/64229

    Signed-off-by: Adam King <adking@redhat.com>

- commit 5c613b3788d9ae686b4dc29d9414674ecb6f6adb
  Author: Roy Sahar <royswi@gmail.com>
  Date:   Thu Feb 8 17:58:43 2024 +0200

    nvmeof: Add mount for log location

    Signed-off-by: Roy Sahar <royswi@gmail.com>

- commit 726050aa3fd1ce2b21539fdd632b16e880bf2946
  Author: Roy Sahar <royswi@gmail.com>
  Date:   Wed Apr 3 12:21:05 2024 +0300

    Ceph NVMEoF Gateway - add mtls folder for nvmeof container

    Signed-off-by: Roy Sahar <royswi@gmail.com>

Resolves: rhbz#2273837

Signed-off-by: Alexander Indenbaum <aindenba@redhat.com>

src/cephadm/cephadm.py
@@ -1190,7 +1190,7 @@ class CephNvmeof(object):
                    fetch_configs(ctx), ctx.image)
 
     @staticmethod
-    def get_container_mounts(data_dir: str) -> Dict[str, str]:
+    def get_container_mounts(data_dir: str, log_dir: str, mtls_dir: Optional[str] = None) -> Dict[str, str]:
         mounts = dict()
         mounts[os.path.join(data_dir, 'config')] = '/etc/ceph/ceph.conf:z'
         mounts[os.path.join(data_dir, 'keyring')] = '/etc/ceph/keyring:z'
@@ -1198,6 +1198,9 @@ class CephNvmeof(object):
         mounts[os.path.join(data_dir, 'configfs')] = '/sys/kernel/config'
         mounts['/dev/hugepages'] = '/dev/hugepages'
         mounts['/dev/vfio/vfio'] = '/dev/vfio/vfio'
+        mounts[log_dir] = '/var/log/ceph:z'
+        if mtls_dir:
+            mounts[mtls_dir] = '/src/mtls:z'
         return mounts
 
     @staticmethod
@@ -1216,7 +1219,7 @@ class CephNvmeof(object):
         out, err, ret = call(ctx,
                              [ctx.container_engine.path, 'inspect',
                               '--format', '{{index .Config.Labels "io.ceph.version"}}',
-                              ctx.image])
+                              container_id])
         version = None
         if ret == 0:
             version = out.strip()
@@ -3620,7 +3623,12 @@ def get_container_mounts(ctx, fsid, daemon_type, daemon_id,
     if daemon_type == CephNvmeof.daemon_type:
         assert daemon_id
         data_dir = get_data_dir(fsid, ctx.data_dir, daemon_type, daemon_id)
-        mounts.update(CephNvmeof.get_container_mounts(data_dir))
+        log_dir = os.path.join(ctx.log_dir, self.identity.fsid)
+        mtls_dir = os.path.join(ctx.data_dir, self.identity.fsid, 'mtls')
+        if os.path.exists(mtls_dir):
+            mounts.update(CephNvmeof.get_container_mounts(data_dir, log_dir, mtls_dir=mtls_dir))
+        else:
+            mounts.update(CephNvmeof.get_container_mounts(data_dir, log_dir))
 
     if daemon_type == CephIscsi.daemon_type:
         assert daemon_id

commit 01358394e09b4cb54c20e97a497a91316d110fca

nvmeof: fix backport commit
Resolves bug introduced in fcfa6e797f021886971e3af97a5158797cd2d96f
There are two 'fsid' references as self scoped variables in upstream,
should be local variables in downstream.

Resolves: rhbz#2274305

Signed-off-by: Alexander Indenbaum <aindenba@redhat.com>

src/cephadm/cephadm.py
@@ -3623,8 +3623,8 @@ def get_container_mounts(ctx, fsid, daemon_type, daemon_id,
     if daemon_type == CephNvmeof.daemon_type:
         assert daemon_id
         data_dir = get_data_dir(fsid, ctx.data_dir, daemon_type, daemon_id)
-        log_dir = os.path.join(ctx.log_dir, self.identity.fsid)
-        mtls_dir = os.path.join(ctx.data_dir, self.identity.fsid, 'mtls')
+        log_dir = os.path.join(ctx.log_dir, fsid)
+        mtls_dir = os.path.join(ctx.data_dir, fsid, 'mtls')
         if os.path.exists(mtls_dir):
             mounts.update(CephNvmeof.get_container_mounts(data_dir, log_dir, mtls_dir=mtls_dir))
         else: