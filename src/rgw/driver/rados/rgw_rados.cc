87533a16b052509ec173989f0b3e25937e0f6942
Marcus Watts <mwatts@redhat.com>
2024-04-15 20:11:03 -0400

    More fixes for first part copy object encryption fixes.
    This fixes the last part of the control path for copy object
    encryption; removing a check that kept this from working.
    
    Resolves: rhbz#2149450
    Resolves: rhbz#2266248
    
    Signed-off-by: Marcus Watts <mwatts@redhat.com>
    (cherry picked from commit a21cbc0a9f63c955cedd19514a99de9af02ccae4)

--- src/rgw/driver/rados/rgw_rados.cc
+++ src/rgw/driver/rados/rgw_rados.cc
@@ -4651,19 +4651,6 @@ int RGWRados::copy_obj(RGWObjectCtx& obj_ctx,
   if (ret < 0) {
     return ret;
   }
-  if (!src_attrs.count(RGW_ATTR_CRYPT_MODE)) {
-#if 0
-  } else if (!(ret = enc_cb.copy_ok(src_obj, src_attrs))) {
-#endif
-  } else {
-    // Current implementation does not follow S3 spec and even
-    // may result in data corruption silently when copying
-    // multipart objects acorss pools. So reject COPY operations
-    //on encrypted objects before it is fully functional.
-    ldpp_dout(dpp, 0) << "ERROR: copy op for encrypted object " << src_obj
-                  << " has not been implemented." << dendl;
-    return -ERR_NOT_IMPLEMENTED;
-  }
 
   src_attrs[RGW_ATTR_ACL] = attrs[RGW_ATTR_ACL];
   src_attrs.erase(RGW_ATTR_DELETE_AT);
